e8eb072dfbe0447f933ae205d67621ca9954bd78
wobahtorsi <wobahtorsi@gmail.com>
2025-01-29 04:15:13 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# paksabocilngentot
-paksabocilngentot
+[paksabocilngentot](https://paksabocilngentot.pages.dev)
+Fork the [repository](https://github.com/tondigule) on GitHub.
---
+++ app/layout.tsx
@@ -0,0 +1,82 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video bokep indo terbaru viral bocil jilbab abg colmek smp indonesia tante mama sma`,
+    metadataBase: new URL("https://paksabocilngentot.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+        description: `${SITENAME} Video bokep indo terbaru viral bocil jilbab abg colmek smp indonesia tante mama sma`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: '9UjvRyi1grjP3L1JUHTJhoUaJyhRhpCPaG6cThQtmi0',
+        yandex: '3ccf4e1014d06ca7',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video bokep indo terbaru viral bocil jilbab abg colmek smp indonesia tante mama sma`,
+        "logo": "https://paksabocilngentot.pages.dev/favicon.ico",
+        "url": "https://paksabocilngentot.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://paksabocilngentot.pages.dev"
+            },
+            "foundingDate":"2025-01-28"
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://paksabocilngentot.pages.dev",
+        "description": `${SITENAME} Video bokep indo terbaru viral bocil jilbab abg colmek smp indonesia tante mama sma`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://paksabocilngentot.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
